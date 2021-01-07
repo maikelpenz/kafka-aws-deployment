fcb92d162c0b816ac57a61514f4ddc02b2027323
Maikel Penz <maikelpenz@gmail.com>
2021-01-07 21:19:29 +1300

    fixing hardcoded az reference

--- aws-infrastructure/modules/kad-vpc/subnets.tf
+++ aws-infrastructure/modules/kad-vpc/subnets.tf
@@ -1,32 +1,34 @@
-resource "aws_subnet" "kad-public-subnet-a" {
-    vpc_id     = aws_vpc.kad-vpc.id
-    availability_zone = "ap-southeast-2a"
-    cidr_block = cidrsubnet(aws_vpc.kad-vpc.cidr_block, 4, 0)
-    map_public_ip_on_launch="true"
+data "aws_region" "current_region" {}
 
-    tags = {
-        Name = "kad-public-subnet-a"
-    }
+resource "aws_subnet" "kad-public-subnet-a" {
+  vpc_id                  = aws_vpc.kad-vpc.id
+  availability_zone       = "${data.aws_region.current_region.name}a"
+  cidr_block              = cidrsubnet(aws_vpc.kad-vpc.cidr_block, 4, 0)
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "kad-public-subnet-a"
+  }
 }
 
 resource "aws_subnet" "kad-public-subnet-b" {
-    vpc_id     = aws_vpc.kad-vpc.id
-    availability_zone = "ap-southeast-2b"
-    cidr_block = cidrsubnet(aws_vpc.kad-vpc.cidr_block, 4, 1)
-    map_public_ip_on_launch="true"
+  vpc_id                  = aws_vpc.kad-vpc.id
+  availability_zone       = "${data.aws_region.current_region.name}b"
+  cidr_block              = cidrsubnet(aws_vpc.kad-vpc.cidr_block, 4, 1)
+  map_public_ip_on_launch = "true"
 
-    tags = {
-        Name = "kad-public-subnet-b"
-    }
+  tags = {
+    Name = "kad-public-subnet-b"
+  }
 }
 
 resource "aws_subnet" "kad-public-subnet-c" {
-    vpc_id     = aws_vpc.kad-vpc.id
-    availability_zone = "ap-southeast-2c"
-    cidr_block = cidrsubnet(aws_vpc.kad-vpc.cidr_block, 4, 2)
-    map_public_ip_on_launch="true"
+  vpc_id                  = aws_vpc.kad-vpc.id
+  availability_zone       = "${data.aws_region.current_region.name}c"
+  cidr_block              = cidrsubnet(aws_vpc.kad-vpc.cidr_block, 4, 2)
+  map_public_ip_on_launch = "true"
 
-    tags = {
-        Name = "kad-public-subnet-c"
-    }
+  tags = {
+    Name = "kad-public-subnet-c"
+  }
 }
